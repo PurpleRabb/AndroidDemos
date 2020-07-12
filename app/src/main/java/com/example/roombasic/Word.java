bf05a3cb9271c175554d9af259aa77f46d2f8e20
liushuo <364725259@qq.com>
2020-07-12 18:18:47 +0800

    添加数据库迁移策略

--- app/src/main/java/com/example/roombasic/Word.java
+++ app/src/main/java/com/example/roombasic/Word.java
@@ -15,6 +15,17 @@ public class Word {
     @ColumnInfo(name = "chinese_meaning")
     private String meaning;
 
+    @ColumnInfo(name = "chinese_invisible")
+    private boolean chineseInvisible;
+
+    public boolean isChineseInvisible() {
+        return chineseInvisible;
+    }
+
+    public void setChineseInvisible(boolean chineseInvisible) {
+        this.chineseInvisible = chineseInvisible;
+    }
+
     public Word(String word, String meaning) {
         this.word = word;
         this.meaning = meaning;
--- app/src/main/java/com/example/roombasic/WordDatabase.java
+++ app/src/main/java/com/example/roombasic/WordDatabase.java
@@ -2,20 +2,30 @@ package com.example.roombasic;
 
 import android.content.Context;
 
+import androidx.annotation.NonNull;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.room.migration.Migration;
+import androidx.sqlite.db.SupportSQLiteDatabase;
 
-@Database(entities = {Word.class}, version = 1, exportSchema = false)
+@Database(entities = {Word.class}, version = 2, exportSchema = false)
 public abstract class WordDatabase extends RoomDatabase {
     private static WordDatabase INSTANCE;
 
     public static synchronized WordDatabase getInstance(Context context) {
         if(INSTANCE == null) {
-            INSTANCE = Room.databaseBuilder(context.getApplicationContext(),WordDatabase.class,"word_database")
+            INSTANCE = Room.databaseBuilder(context.getApplicationContext(),WordDatabase.class,"word_database").addMigrations(MIGRATION_1_2)
             .build();
         }
         return INSTANCE;
     }
     public abstract WordDao getWordDao();
+
+    static final Migration MIGRATION_1_2 = new Migration(1,2) {
+        @Override
+        public void migrate(@NonNull SupportSQLiteDatabase database) {
+            database.execSQL("ALTER TABLE word ADD COLUMN chinese_invisible INTEGER NOT NULL DEFAULT 0");
+        }
+    };
 }
