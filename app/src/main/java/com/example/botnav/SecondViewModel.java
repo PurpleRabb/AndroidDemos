8886b1310bf5241989a9b5e9e663aae5c7a59f44
liushuo <364725259@qq.com>
2020-07-16 20:03:17 +0800

    Add bottom navigation bar
    注意navigation里的id要跟menu里面的id一致

---
+++ app/src/main/java/com/example/botnav/SecondViewModel.java
@@ -0,0 +1,7 @@
+package com.example.botnav;
+
+import androidx.lifecycle.ViewModel;
+
+public class SecondViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/example/botnav/ThirdViewModel.java
@@ -0,0 +1,7 @@
+package com.example.botnav;
+
+import androidx.lifecycle.ViewModel;
+
+public class ThirdViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
